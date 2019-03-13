7737a69ecfa72a38d53f7cd71faa67dd254d4406
Topiltzin <nokoayzack@gmail.com>
2019-03-12 21:40:14 -0600

    Estructura básica del proyecto

--- app.js
+++ app.js
@@ -1,28 +1,43 @@
 require('dotenv').config();
 
 const express = require('express');
-const hbs = require('hbs');
+const hbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const sassMiddleware  = require('node-sass-middleware');
 const path = require('path');
+const logger = require('morgan');
 
 const app = express();
 
+// Middleware
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 // Setup View Engine
-app.use(require('node-sass-middleware')({
+app.use(sassMiddleware({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
-  sourceMap: true
-}));
+  debug: true,
+  outputStyle: 'compressed'
+}), express.static(path.join(__dirname, 'public')));
 
 app.set('views', path.join(__dirname, 'views'));
+
 app.set('view engine', 'hbs');
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.engine('hbs', hbs({
+  extname: 'hbs',
+  defaultLayout: 'base',
+  layoutsDir: path.join(__dirname, 'views', 'layouts'),
+  partialsDir: path.join(__dirname, 'views', 'partials')
+}));
 
 app.locals.title = 'Cáritas de Querétaro';
 
 app.get('/', (req, res) => {
-  res.send('Bien, bien');
+  res.render('index/index');
 });
 
 const port=process.env.PORT || 3000
