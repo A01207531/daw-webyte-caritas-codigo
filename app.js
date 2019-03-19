231f1edd2a19df1d6c0a1ccf989d7121dc1c755d
Topiltzin <nokoayzack@gmail.com>
2019-03-19 14:07:07 -0600

    cambiamos bcrypt por bryptjs

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const path = require('path');
 const db = require('./models');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const dashboardRouter = require('./dashboard');
 const app = express();
