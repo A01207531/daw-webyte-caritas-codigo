99a060b15b74374700bffd77eaf7dcd3190fcf25
Topiltzin <nokoayzack@gmail.com>
2019-03-19 14:37:49 -0600

    constantes añadidas para rbac

---
+++ constants/rbac.js
@@ -0,0 +1,12 @@
+export const CREATE_USER = "crearUsuario";
+export const READ_USER = "consultarUsuarios";
+export const UPDATE_USER = "modificarUsuario";
+export const DELETE_USER = "eliminarUsuario";
+
+export const CREATE_PROJECT = "crearProyecto";
+export const UPDATE_USER = "modificarProyecto";
+
+export const CREATE_BOX = "crearBuzon";
+export const UPDATE_BOX = "modificarBuzon";
+
+export const DONATE = "realizarDonativo";
