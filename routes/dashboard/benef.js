ebf1a6a248da134e594833eb5537c8147f023d06
Eduardo Castillo <eduardoandrescp@outlook.com>
2019-03-20 09:15:21 -0600

    La tabla de beneficiarios funciona

--- routes/dashboard/benef.js
+++ routes/dashboard/benef.js
@@ -1,15 +1,24 @@
+const db = require('../../models');
+
 const benef = require('express').Router();
 
 benef.get('/', async (req, res) => {
 	if(!req.session.userID){
 		res.redirect("/login");
 		return;
-	}
+    }
+    
+    //Ask the beneficiaries to the db
+    const bq = await db.query('SELECT nombre,apellido,curp,sexo,nacimiento FROM beneficiario');
+    console.log(bq);
+
+    //res.json(bq.rows);
 	
 	res.render('dashboard/list-benef',{
 		layout: 'dashboard-base',
-		user: req.session.user
-	})
+        user: req.session.user,
+        benef: bq.rows
+	});
 });
 
 benef.get('/nuevo', async (req, res) => {
