98060c0c417eb7741acc709b2450776ae886a60c
Topiltzin <nokoayzack@gmail.com>
2019-03-19 20:44:16 -0600

    añadida ruta para proyectos

---
+++ routes/proyectos/index.js
@@ -0,0 +1,14 @@
+const router = require('express').Router();
+
+router.get('/', async (req, res) => {
+
+  const proyectos = [
+    { nombre: 'Proyecto 1', descripcion: 'ESta es la descripción del proyecto 1.' },
+    { nombre: 'Proyecto 2', descripcion: 'ESta es la descripción del proyecto 2.' },    
+    { nombre: 'Proyecto 3', descripcion: 'ESta es la descripción del proyecto 3.' },    
+  ];
+
+  res.render('proyectos/index', { proyectos });
+});
+
+module.exports = router;
