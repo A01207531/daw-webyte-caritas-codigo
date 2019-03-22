de5d07804afad3c8de50bbc5544931c4176d2eaa
Eduardo Andrés Castillo Perera <eduardoandrescp@outlook.com>
2019-03-22 15:57:41 -0600

    Iniciado dropdown dinamico para estados

---
+++ public/javascripts/mx-data.js
@@ -0,0 +1,20 @@
+const mxStates = [
+    [
+        {name: "Cd. XD",id: 1},
+        {name: "Cd. LOL",id: 2},
+        {name: "Cd. Pacman",id: 3},
+        {name: "Cd. Pikachu",id: 4},
+    ],
+    [
+        {name: "Hidrogeno",id: 11},
+        {name: "Helio",id: 12},
+        {name: "Oxigeno",id: 13},
+        {name: "Mercurio",id: 14},
+    ],
+    [
+        {name: "MErcurio",id: 21},
+        {name: "Venus",id: 22},
+        {name: "Tierra",id: 23},
+        {name: "Marte",id: 24},   
+    ]
+]
