36b81edab7fefaf863ac33a83f26d6a8bd522a6a
Eduardo Castillo <eduardoandrescp@outlook.com>
2019-03-22 20:05:07 -0600

    select municipio dinamico funciona

--- public/javascripts/new-proj-form.js
+++ public/javascripts/new-proj-form.js
@@ -1,2 +1,24 @@
 
 $('select').formSelect();
+
+
+const first = mx[$('#state').val()];
+
+for(const c in first){
+    const city = first[c];
+    $('#city').append('<option value="'+city.id+'">'+city.nombre+'</option>');
+}
+
+$('#state').change(e => {
+    //get the cities
+    const cities = mx[$('#state').val()];
+    //we have the cities
+    //clear the select
+    $('#city').empty();
+    //populate the cities
+    for(const c in cities){
+        const city = cities[c];
+        $('#city').append('<option value="'+city.id+'">'+city.nombre+'</option>');
+    }
+
+})
