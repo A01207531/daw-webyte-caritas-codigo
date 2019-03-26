4ebf854367d27935a039087c7a795c9e3179c8dc
Eduardo Castillo <eduardoandrescp@outlook.com>
2019-03-25 21:26:55 -0600

    Funciona listado de canalizaciones

--- routes/dashboard/canalizaciones.js
+++ routes/dashboard/canalizaciones.js
@@ -1,16 +1,23 @@
+const db = require('../../models');
 const can = require('express').Router();
 
-can.get('/', (req,res) => {
+can.get('/', async (req,res) => {
     if(!req.session.userID){
 		res.redirect("/login");
 		return;
-    }
-    
+		}
+		
+		//En esta hacemos un query sencillo a las canalizaciones
+		const cq = await db.query('SELECT * FROM canalizacion ORDER BY contacto');
+
+		console.log(cq.rows);
+		
     //por mientras
     res.render('dashboard/list-canalizaciones',{
-		layout: 'dashboard-base',
-		user: req.session.user
-	})
+			layout: 'dashboard-base',
+			user: req.session.user,
+			canalizaciones: cq.rows
+		})
 })
 
 
