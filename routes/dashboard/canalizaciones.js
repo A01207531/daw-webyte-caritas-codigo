c6cec7dc36a2164e5c4b047d4208cbb4c971a657
Eduardo Castillo <eduardoandrescp@outlook.com>
2019-03-25 22:06:46 -0600

    Posible validación de numeros telefonicos

--- routes/dashboard/canalizaciones.js
+++ routes/dashboard/canalizaciones.js
@@ -1,6 +1,9 @@
 const db = require('../../models');
 const can = require('express').Router();
 
+
+const phoneRegEx = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+
 can.get('/', async (req,res) => {
     if(!req.session.userID){
 		res.redirect("/login");
@@ -26,6 +29,8 @@ can.get('/nueva', async (req,res) => {
 		res.redirect("/login");
 		return;
 	}
+
+	//console.log(phoneRegEx.test('(999) 998 5754'));
 	
 	res.render('dashboard/new-canalizacion',{
 		layout: 'dashboard-base',
