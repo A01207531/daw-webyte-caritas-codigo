1f238734357cfabd9cd27d3e8a784b8bc667cead
Eduardo Castillo <eduardoandrescp@outlook.com>
2019-03-27 15:49:36 -0600

    Arreglar views controladores beneficiarios

--- routes/dashboard/benef.js
+++ routes/dashboard/benef.js
@@ -13,7 +13,7 @@ benef.get('/', async (req, res) => {
 
     //res.json(bq.rows);
 	
-	res.render('dashboard/beneficiarios/list-benef',{
+	res.render('dashboard/beneficiarios/list',{
 		layout: 'dashboard-base',
         user: req.session.user,
         benef: bq.rows
@@ -27,7 +27,7 @@ benef.get('/nuevo', async (req, res) => {
 	}
 	
 
-	res.render('dashboard/new-benef',{
+	res.render('dashboard/beneficiarios/create',{
 		layout: 'dashboard-base'
 	});
 
