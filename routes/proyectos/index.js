d2a57c8a6c2ed295a2ce960cce820b6f0edb2ed9
Topiltzin <nokoayzack@gmail.com>
2019-03-26 20:51:04 -0600

    la vista proyectos/detalle ahora muestra toda la información del proyecto solicitado; si no existe tal proyecto, manda error 404

--- routes/proyectos/index.js
+++ routes/proyectos/index.js
@@ -12,17 +12,20 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   let proyecto = await db.query('SELECT * FROM proyecto WHERE id=$1', [req.params.id]);
-  proyecto = proyecto.rows[0];
-
-  let [ subPrograma, municipio ] = await Promise.all([db.query('SELECT * FROM subprograma WHERE id=$1', [proyecto.subprograma_id]), db.query('SELECT * FROM municipio WHERE id=$1', [proyecto.municipio_id])]);
-  proyecto.municipio = municipio.rows[0];
-  // subPrograma = subPrograma.rows[0];
+  if(proyecto.rowCount>0) {
+    proyecto = proyecto.rows[0];
   
-  let programa = await db.query('SELECT * FROM programa WHERE id=$1', [subPrograma.rows[0].programa_id]);
-  proyecto.programa = programa.rows[0];
-  proyecto.programa.subPrograma = subPrograma.rows[0];
-
-  console.log(programa);
-  res.render('proyectos/detalle', { proyecto, session: req.session });
+    let [ subPrograma, municipio ] = await Promise.all([db.query('SELECT * FROM subprograma WHERE id=$1', [proyecto.subprograma_id]), db.query('SELECT * FROM municipio WHERE id=$1', [proyecto.municipio_id])]);
+    proyecto.municipio = municipio.rows[0];
+    // subPrograma = subPrograma.rows[0];
+    
+    let programa = await db.query('SELECT * FROM programa WHERE id=$1', [subPrograma.rows[0].programa_id]);
+    proyecto.programa = programa.rows[0];
+    proyecto.programa.subPrograma = subPrograma.rows[0];
+  
+    res.render('proyectos/detalle', { statys:'ok', proyecto, session: req.session });
+  } else {
+    res.render('404', { status:'err', err:'No se pudo encontrar el proyecto solicitado', session: req.session });
+  }
 });
 module.exports = router;
