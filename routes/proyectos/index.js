91b48f2875aca6cb62cf237d81216532ea221ee8
Topiltzin <nokoayzack@gmail.com>
2019-03-26 19:31:46 -0600

    se modificó el controlador proyectos/index.js en GET /:id para mostrar información de subprograma y municipio

--- routes/proyectos/index.js
+++ routes/proyectos/index.js
@@ -13,7 +13,11 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   let proyecto = await db.query('SELECT * FROM proyecto WHERE id=$1', [req.params.id]);
   proyecto = proyecto.rows[0];
-  // console.log(proyecto)
-  res.render('proyectos/detalle', { proyecto, session: req.session });
+
+  let [ subPrograma, municipio ] = await Promise.all([db.query('SELECT * FROM subprograma WHERE id=$1', [proyecto.subprograma_id]), db.query('SELECT * FROM municipio WHERE id=$1', [proyecto.municipio_id])]);
+  subPrograma = subPrograma.rows[0];
+  municipio = municipio.rows[0];
+  // res.render('proyectos/detalle', { proyecto, session: req.session });
+  res.json({proyecto, subPrograma, municipio});
 });
 module.exports = router;
