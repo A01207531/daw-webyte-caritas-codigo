305f43e13b1ebaeb8489bf86bbd164d489ddc74e
Eduardo Castillo <eduardoandrescp@outlook.com>
2019-04-23 13:55:42 -0500

    El blob storage funciona

--- routes/dashboard/blogadmin.js
+++ routes/dashboard/blogadmin.js
@@ -1,6 +1,20 @@
 const db = require('../../models');
 const r = require('express').Router();
 
+const multer = require('multer');
+const inMemoryStorage = multer.memoryStorage();
+uploadStrategy = multer({ storage: inMemoryStorage }).single('upload');
+const azureStorage = require('azure-storage');
+const blobService = azureStorage.createBlobService();
+const getStream = require('into-stream');
+const containerName = 'uploads';
+
+const getBlobName = originalName => {
+	const identifier = Math.random().toString().replace(/0\./, ''); // remove "0." from start of string
+	return `${identifier}-${originalName}`;
+};
+
+
 //esto deberia listar los posts en una tabla
 r.get('/', async (req,res) => {
     if(!req.session.userID){
@@ -35,10 +49,59 @@ r.get('/nuevo', (req,res) => {
 	})
 })
 
+const handleError = (err, res) => {
+	res.status(500);
+	res.render('error', { error: err });
+};
+
+function savePostInDB(title,content,img,req,res){
+	const query = 'INSERT INTO posts(titulo,cuerpo,fotourl,autor) VALUES ($1,$2,$3,$4)';
+	const values = [title,content,"https://caritasqro.blob.core.windows.net/uploads/"+img,req.session.userID];
+
+	db.query(query,values, (err, resp) => {
+		if(err){
+			console.log(err.stack);
+		  //Este error viene de la BD, por lo que solo puede ser por la
+		  //violación de la llave única. 
+		  res.render('dashboard/errors/generic',{
+			  layout: 'dashboard-base',
+			  user: req.session.user,
+			  title: 'Error al ingresar los datos',
+			  text: 'Ocurrio un error al insertar la imagen'
+		  });
+		}else{
+			res.render('dashboard/canalizaciones/success',{
+				layout: 'dashboard-base',
+				user: req.session.user,
+			})
+		}
+	})
+}
+
 //esto es el endpoint del form
-r.post('/nuevo', (req,res) => {
+r.post('/nuevo',uploadStrategy,(req,res) => {
 	
 	//res.json(req.body)
+	const title = req.body.titulo;
+	const content = req.body.contenido;
+
+	//azure stuff
+	const
+          blobName = getBlobName(req.file.originalname)
+        , stream = getStream(req.file.buffer)
+        , streamLength = req.file.buffer.length
+		;
+		
+		blobService.createBlockBlobFromStream(containerName, blobName, stream, streamLength, err => {
+
+			if(err) {
+					handleError(err);
+					return;
+			}
+
+			//we did it. Now store the rest of the data in the DB
+			savePostInDB(title,content,blobName,req,res);
+	});
 
 	/*db.query(query,values, (err, resp) => {
 		if(err){
