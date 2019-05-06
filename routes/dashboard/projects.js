68ba864c4925d6c2c48ce612f6b4b56547bd1058
Eduardo Andrés Castillo Perera <eduardoandrescp@outlook.com>
2019-05-06 12:00:32 -0500

    json endpoint for dinamic

--- routes/dashboard/projects.js
+++ routes/dashboard/projects.js
@@ -135,4 +135,33 @@ pr.get('/programAPI',async (req, res) => {
 	res.json(q.rows);
 })
 
+//--Edit
+pr.get('/json/:id',async (req,res) => {
+	let proyecto = await db.query('SELECT * FROM proyecto WHERE id=$1', [req.params.id]);
+  if(proyecto.rowCount>0) {
+    proyecto = proyecto.rows[0];
+  
+    let [ subPrograma, municipio, totalDonadores, totalDonaciones ] = await Promise.all([
+      db.query('SELECT * FROM subprograma WHERE id=$1', [proyecto.subprograma_id]), 
+      db.query('SELECT * FROM municipio WHERE id=$1', [proyecto.municipio_id]),
+      db.query('SELECT COUNT(nombre) FROM donasporpersona'),
+      db.query('SELECT SUM(monto) FROM dona WHERE proyecto_id=$1', [req.params.id])
+    ]);
+    proyecto.municipio = municipio.rows[0];
+    // subPrograma = subPrograma.rows[0];
+    let programa = await db.query('SELECT * FROM programa WHERE id=$1', [subPrograma.rows[0].programa_id]);
+    proyecto.programa = programa.rows[0];
+    proyecto.programa.subPrograma = subPrograma.rows[0];
+    
+    let paypal_string = process.env.paypal_string;
+    totalDonadores = totalDonadores.rows[0].count;
+    totalDonaciones = totalDonaciones.rows[0].sum;
+
+    // console.log("Donadores: ", totalDonadores, "Donaciones: ", totalDonaciones)
+    res.json(proyecto);
+  } else {
+    res.render('404', { status:'err', err:'No se pudo encontrar el proyecto solicitado', session: req.session });
+  }
+})
+
 module.exports = pr;
