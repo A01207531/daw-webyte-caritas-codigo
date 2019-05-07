66d0540ab00cbed0ad57080e06fdd39cbad65702
JavierIQ <a01366111@itesm.mx>
2019-05-07 03:07:32 -0500

    Contacto terminado

--- routes/dashboard/projects.js
+++ routes/dashboard/projects.js
@@ -14,7 +14,7 @@ const getBlobName = originalName => {
 	const identifier = Math.random().toString().replace(/0\./, ''); // remove "0." from start of string
 	return `${identifier}-${originalName}`;
 };
-
+ 
 pr.get('/', async (req, res) => {
 	if(!req.session.userID){
 		res.redirect("/login");
