1d067320e9d63d0fac1673935b26ba8aee1962c5
Topiltzin <nokoayzack@gmail.com>
2019-05-08 13:50:49 -0500

    lista edición de beneficiario

--- routes/dashboard/benef.js
+++ routes/dashboard/benef.js
@@ -207,13 +207,11 @@ benef.get('/json/:id',async (req,res) => {
 	const pids = await db.query(projectIdsQuery,[req.params.id]);
 
 	if(!req.session.userID){
-		    res.redirect("/login");
-		    return;
-		  }
-		  if(req.session.user.privileges.includes(CONSTANTS.CREATE_PROJECT)) {
+    res.redirect("/login");
+    return;
+  }
+  if(req.session.user.privileges.includes(CONSTANTS.CREATE_PROJECT)) {
 		
-		
-
 	let beneficiario = await db.query('SELECT * FROM beneficiario WHERE id=$1', [req.params.id]);
   if(beneficiario.rowCount>0) {
 		beneficiario = beneficiario.rows[0];
@@ -243,65 +241,49 @@ benef.post('/modificar/:id', (req,res) => {
 	console.log(req.body.proyectos);
 
 	if(!req.session.userID){
-		    res.redirect("/login");
-		    return;
-		  }
-		  if(req.session.user.privileges.includes(CONSTANTS.CREATE_PROJECT)) {
+    res.redirect("/login");
+    return;
+  }
+  if(req.session.user.privileges.includes(CONSTANTS.CREATE_PROJECT)) {
 		
-		
-
-	const name = req.body.name;
-	const lastname = req.body.lastname;
-	let checkIndigena = req.body.checkIndigena;
-	let checkExtrangero = req.body.checkExtrangero;
-	const state = req.body.state;
-	const city = req.body.city;
-	const nacimiento = req.body.nacimiento;
-	const address = req.body.address;
-	const curp = req.body.curp;
-	const rfc = req.body.rfc;
-	const profesion = req.body.profesion;
-	const status = req.body.status;
-	const estadoCivil = req.body.estadoCivil;
-	const canalizacion = req.body.canalizacion;
+		let { calle, numero, cp, colonia, profesion, name, lastname, curp, sexo, nacimiento, city, canalizacion, rfc, estadoCivil, zona, checkIndigena, checkExtrangero } = req.body;
 	
-	if (checkIndigena==null) {
-		checkIndigena=false;
-	}
-	if (checkExtrangero==null) {
-		checkExtrangero=false;
-	}
 	
-	const params = [name,lastname,checkIndigena,checkExtrangero,nacimiento,address,curp,rfc,profesion,status,estadoCivil,canalizacion,req.params.id];
-	console.log(...params);
-	const query = 'UPDATE beneficiario SET nombre=$1,apellido=$2,indigente=$3,extranjero=$4,nacimiento=$5,direccion=$6,curp=$7,rfc=$8,profesion=$9,sexo=$10,estadocivil=$11,canalizacion_id=$12 WHERE id=$13';
+		checkIndigena = !!checkIndigena;
+		checkExtrangero = !!checkExtrangero;
 
-	//Reassociate projects with beneficiaries
-	reasoc(req.params.id,req.body.proyectos);
+		const params = [name,lastname,curp,sexo,nacimiento,city,canalizacion,rfc,estadoCivil,zona,checkExtrangero,checkIndigena,profesion,calle,numero,cp,colonia,req.params.id];
+			
+
+		console.log(...params);
+		const query = 'UPDATE beneficiario SET nombre=$1,apellido=$2,curp=$3,sexo=$4,nacimiento=$5,municipio_id=$6,canalizacion_id=$7,rfc=$8,estadocivil=$9,zonageografica=$10,extranjero=$11,indigente=$12,profesion=$13,calle=$14,numero=$15,cp=$16,colonia=$17 WHERE id=$18';
+
+		//Reassociate projects with beneficiaries
+		reasoc(req.params.id,req.body.proyectos);
 
 
-	db.query(query,params, (err, resp) => {
-		if(err){
-			console.log(err.stack);
-		  //Este error viene de la BD, por lo que solo puede ser por la
-		  //violación de la llave única. 
-		  res.render('dashboard/errors/generic',{
-			  layout: 'dashboard-base',
-			  user: req.session.user,
-			  title: 'Error al ingresar los datos',
-			  text: 'Ocurrio un error al insertar los datos'
-		  });
-		}else{
-			res.render('dashboard/beneficiarios/modificado',{
-				layout: 'dashboard-base',
-				user: req.session.user,
+		db.query(query,params, (err, resp) => {
+			if(err){
+				console.log(err.stack);
+				//Este error viene de la BD, por lo que solo puede ser por la
+				//violación de la llave única. 
+				res.render('dashboard/errors/generic',{
+					layout: 'dashboard-base',
+					user: req.session.user,
+					title: 'Error al ingresar los datos',
+					text: 'Ocurrio un error al insertar los datos'
+				});
+			}else{
+				res.render('dashboard/beneficiarios/modificado',{
+					layout: 'dashboard-base',
+					user: req.session.user,
 				})
-		}
-	})
-}else{
+			}
+		})
+	}else{
 	  res.redirect('/dashboard');
 	  return;
-	  }
+  }
 })
 
 module.exports = benef;
